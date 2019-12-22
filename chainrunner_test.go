0a1afc9aef811ec3d8600859e7cab1f955821a33
Peresada Egor <egor.peresada@domru.ru>
2019-12-22 12:46:28 +0500

    refactor tests

--- chainrunner_test.go
+++ chainrunner_test.go
@@ -1,41 +1,200 @@
 package chainrunner
 
 import (
+	"bytes"
+	"fmt"
 	"github.com/stretchr/testify/assert"
+	"golang.org/x/crypto/ssh"
+	"io/ioutil"
 	"log"
+	"os/exec"
+	"strings"
 	"testing"
 )
 
-func TestRunnerExecute(t *testing.T) {
+func TestChainRunner(t *testing.T) {
 	log.SetFlags(log.Lshortfile)
-	type testCase struct {
-		source    string
-		checkFunc func(t *testing.T)
+	cases := []*TestCase{
+		{t,
+			"SimpleChain",
+			map[string]interface{}{"source": "test/chains/simpleChain.yaml"},
+			pass,
+			initFromFile,
+			assertSimpleChain,
+			pass,
+		},
+		{t,
+			"SimpleChainRevert",
+			map[string]interface{}{"source": "test/chains/simpleChainRevert.yaml"},
+			pass,
+			initFromFile,
+			pass,
+			pass,
+		},
+		{t,
+			"chainOnRemoteHost",
+			map[string]interface{}{"source": "test/chains/chainOnRemoteHost.yaml"},
+			runEnvironment,
+			initFromString,
+			assertChainOnRemoteHost,
+			stopEnvironment,
+		},
+		{t,
+			"chainOnRemoteHostRevert",
+			map[string]interface{}{"source": "test/chains/chainOnRemoteHostRevert.yaml"},
+			pass,
+			initFromFile,
+			pass,
+			pass,
+		},
 	}
-	cases := []testCase{
-		{"test/chains/simpleChain.yaml", func(t *testing.T) {
-			return
-		}},
-		{"test/chains/simpleChainRevert.yaml", func(t *testing.T) {
-			return
-		}},
-		{"test/chains/chainWithSshConnections.yaml", func(t *testing.T) {
-			return
-		}},
-		{"test/chains/chainWithSshConnectionsRevert.yaml", func(t *testing.T) {
-			return
-		}},
-	}
-
-	for _, chain := range cases {
-		t.Run(chain.source, func(t *testing.T) {
-			provider, err := NewYamlProviderFromFile(chain.source)
-			assert.Nil(t, err)
-			runner := provider.CreateChain()
-			assert.NotNil(t, runner)
-			err = runner.Execute()
-			assert.Nil(t, err)
-			chain.checkFunc(t)
+	for _, testCase := range cases {
+		t.Run(testCase.name, func(t *testing.T) {
+			assert.True(t, testCase.before(testCase))
+			defer testCase.after(testCase)
+			assert.True(t, testCase.init(testCase))
+			assert.True(t, testCase.assert(testCase))
 		})
 	}
+}
+
+func assertChainOnRemoteHost(tc *TestCase) (result bool) {
+	testEnv, ok := tc.artifacts["environment"].(*testEnvironment)
+	assert.True(tc.t, ok)
+	port, err := testEnv.GetSshPort()
+	assert.NotEmpty(tc.t, port)
+	assert.Nil(tc.t, err)
+	config := &ssh.ClientConfig{
+		Config:          ssh.Config{},
+		User:            "root",
+		Auth:            []ssh.AuthMethod{ssh.Password("password")},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+		Timeout:         0,
+	}
+	client, err := ssh.Dial("tcp", fmt.Sprintf("127.0.0.1:%s", port), config)
+	if assert.Nil(tc.t, err) {
+		defer client.Close()
+	}
+	session, err := client.NewSession()
+	if assert.Nil(tc.t, err) {
+		defer session.Close()
+	}
+	buf := bytes.NewBuffer([]byte(``))
+	errors := bytes.NewBuffer([]byte(``))
+	session.Stdout = buf
+	session.Stderr = errors
+	err = session.Run("cat chainrunner")
+	assert.Nil(tc.t, err)
+	assert.Equal(tc.t, "", errors.String())
+	result = assert.Contains(tc.t, buf, "line was added on container")
+	return
+}
+
+func assertSimpleChain(tc *TestCase) (result bool) {
+	for _, value := range []string{"TestSimpleChain", "TestSimpleChain2"} {
+		result = func(fileName string) bool {
+			data, err := ioutil.ReadFile(fileName)
+			assert.Nil(tc.t, err)
+			actual := data[:len(data)-1] // delete "\n" in the end
+			return assert.Equal(tc.t, []byte(fileName), actual)
+		}(value)
+		if result == false {
+			return
+		}
+	}
+
+	return
+}
+
+func initFromString(tc *TestCase) (result bool) {
+	source := tc.artifacts["source"].(string)
+	provider, err := NewYamlProviderFromString(source)
+	assert.Nil(tc.t, err)
+	runner := provider.CreateChain()
+	assert.NotNil(tc.t, runner)
+	err = runner.Execute()
+	result = assert.Nil(tc.t, err)
+	return
+}
+
+func initFromFile(tc *TestCase) (result bool) {
+	source := tc.artifacts["source"].(string)
+	provider, err := NewYamlProviderFromFile(source)
+	assert.Nil(tc.t, err)
+	runner := provider.CreateChain()
+	assert.NotNil(tc.t, runner)
+	err = runner.Execute()
+	result = assert.Nil(tc.t, err)
+	return
+}
+
+func runEnvironment(tc *TestCase) (result bool) {
+	testEnv := NewTestEnvironment()
+	err := testEnv.Start()
+	assert.Nil(tc.t, err)
+	tc.artifacts["environment"] = testEnv
+	data, err := readFile(NewLocalHost(), tc.artifacts["source"].(string))
+	assert.Nil(tc.t, err)
+	port, err := testEnv.GetSshPort()
+	result = assert.Nil(tc.t, err)
+	tc.artifacts["source"] = strings.Replace(string(data), "{{port}}", port, -1)
+	return
+}
+
+func stopEnvironment(tc *TestCase) (result bool) {
+	testEnv, ok := tc.artifacts["environment"].(*testEnvironment)
+	if !ok {
+		tc.t.Error("can not get TestEnvironment link from artifacts")
+	}
+	err := testEnv.Close()
+	result = assert.Nil(tc.t, err)
+	return
+}
+
+type TestCaseStep func(tc *TestCase) (result bool)
+
+type TestCase struct {
+	t         *testing.T
+	name      string
+	artifacts map[string]interface{}
+	before    TestCaseStep
+	init      TestCaseStep
+	assert    TestCaseStep
+	after     TestCaseStep
+}
+
+func pass(tc *TestCase) bool { return true }
+
+func NewTestEnvironment() *testEnvironment {
+	return &testEnvironment{}
+}
+
+type testEnvironment struct {
+	containerName string
+}
+
+func (t *testEnvironment) Start() error {
+	err := exec.Command("/bin/sh", "-c", fmt.Sprintf("docker build -t chainrunner_ssh_server:latest -f test/sshServerImage/Dockerfile .")).Run()
+	if err != nil {
+		return err
+	}
+	data, err := getShellResult("docker run -d -P chainrunner_ssh_server", NewLocalHost())
+	t.containerName = strings.Replace(string(data), "\n", "", -1)
+
+	return err
+}
+
+func (t *testEnvironment) GetSshPort() (string, error) {
+	data, err := getShellResult(fmt.Sprintf("docker port %s 22", t.containerName), NewLocalHost())
+	if err != nil {
+		return "", err
+	}
+	parts := strings.Split(string(data), ":")
+
+	return strings.Replace(parts[1], "\n", "", -1), nil
+}
+
+func (t *testEnvironment) Close() error {
+	exec.Command("/bin/sh", "-c", "docker stop "+t.containerName).Run()
+	return exec.Command("/bin/sh", "-c", "docker rm "+t.containerName).Run()
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module github.com/egorperesada/chainrunner
 go 1.13
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/stretchr/testify v1.4.0
+	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2
+	golang.org/x/sys v0.0.0-20190507160741-ecd444e8653b // indirect
 	gopkg.in/yaml.v2 v2.2.7
 )
